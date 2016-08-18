155e76906e3b924d25a2375a60d98cc1ed6bb094
aRefox <a.kyrychuk@gmail.com>
2016-08-18 17:50:03 +0300

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,4 +9,4 @@ curl -sSfL "https://github.com/jelastic-jps/fail2ban/raw/master/settings/postgre
 
 sed -i "s~destemail = root@localhost~destemail = ${user.email}~g" /etc/fail2ban/jail.conf
 
-for logfile in $(grep 'logpath =' /etc/fail2ban/jail.tmp | grep -v '%' | awk -F '=' '{ print $2 }' | uniq); do ls $logfile > /dev/null 2>&1 || sed -i 's~'"$(echo $logfile | sed 's~[\.*^$]~\\&~g')"'~&\nenabled = false~g' /etc/fail2ban/jail.tmp; done;
+for logfile in $(grep 'logpath =' /etc/fail2ban/jail.tmp | grep -v '%' | awk -F '=' '{ print $2 }' | uniq); do ls $logfile > /dev/null 2>&1 || sed -i "s~$(echo $logfile | sed 's~[\.*^$]~\\&~g')~&\nenabled = false~g" /etc/fail2ban/jail.tmp; done;

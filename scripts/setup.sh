e2f4cfc2d950e63f989e75ff41afa328877ac319
aRefox <a.kyrychuk@gmail.com>
2016-08-18 19:15:07 +0300

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,5 +9,5 @@ curl -sSfL "https://github.com/jelastic-jps/fail2ban/raw/master/settings/glassfi
 curl -sSfL "https://github.com/jelastic-jps/fail2ban/raw/master/settings/mongodb-iptables.conf" -o /etc/fail2ban/filter.d/mongodb-iptables.conf 2>&1
 curl -sSfL "https://github.com/jelastic-jps/fail2ban/raw/master/settings/postgresql-iptables.conf" -o /etc/fail2ban/filter.d/postgresql-iptables.conf 2>&1
 curl -sSfL "https://github.com/jelastic-jps/fail2ban/raw/master/settings/postgresql.conf" -o /etc/fail2ban/filter.d/postgresql.conf 2>&1
-
 sed -i "s~destemail = root@localhost~destemail = $useremail~g" /etc/fail2ban/jail.conf
+for logfile in $(grep 'logpath =' /etc/fail2ban/jail.tmp | grep -v '%' | awk -F '=' '{ print $2 }' | uniq); do ls $logfile > /dev/null 2>&1 || sed -i "s~$(echo $logfile | sed 's~[\.*^$]~\\&~g')~&\nenabled = false~g" /etc/fail2ban/jail.tmp; done;

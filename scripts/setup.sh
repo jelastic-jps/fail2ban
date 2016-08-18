b94652c66b171c521781fd49bba65adbbf44d147
aRefox <a.kyrychuk@gmail.com>
2016-08-18 23:41:12 +0300

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,7 @@
 useremail=$1
 
 echo "PATH='$(whereis fail2ban-client | awk -F ':' '{print $2}' | awk -F '/fail2ban-client' '{print $1}' | cut -c 2-)'" >> /.bashrc && echo "export PATH" >> /.bashrc
-curl -sSfL "https://github.com/jelastic-jps/fail2ban/raw/master/settings/jail.conf" -o /etc/fail2ban/jail.conf 2>&1"
+curl -sSfL "https://github.com/jelastic-jps/fail2ban/raw/master/settings/jail.conf" -o /etc/fail2ban/jail.conf 2>&1
 curl -sSfL "https://github.com/jelastic-jps/fail2ban/raw/master/settings/paths-jelastic.conf" -o /etc/fail2ban/paths-jelastic.conf 2>&1
 curl -sSfL "https://github.com/jelastic-jps/fail2ban/raw/master/settings/apache-myadmin.conf" -o /etc/fail2ban/filter.d/apache-myadmin.conf 2>&1
 curl -sSfL "https://github.com/jelastic-jps/fail2ban/raw/master/settings/glassfish3-admin.conf" -o /etc/fail2ban/filter.d/glassfish3-admin.conf 2>&1

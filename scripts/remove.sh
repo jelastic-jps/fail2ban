e2cb0882fbb72e09167a73d96b6a19caa7b747a6
aRefox <a.kyrychuk@gmail.com>
2016-08-19 17:16:44 +0300

    Update remove.sh

--- scripts/remove.sh
+++ scripts/remove.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 /etc/init.d/fail2ban stop
-less /etc/fail2ban/uninstall.txt | sed "s~fail2ban.*~fail2ban~g" | xargs rm -rf
+cat /etc/fail2ban/uninstall.txt | xargs rm -rf
 rm -rf /var/lib/fail2ban/fail2ban.sqlite3

4a43898cb22dd11c0f9e572f67b31aac35e5a51e
alexey.lazarenko <dzotic9@gmail.com>
2023-07-17 16:08:24 +0300

    JE-57401 [fail2ban,ftp add-on] addon not works after redeploying containers

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@ mkdir -p /etc/fail2ban
 cd /tmp && rm -rf fail2ban
 curl -sSfL "https://github.com/fail2ban/fail2ban/archive/master.zip" -o /tmp/master.zip 2>&1
 cd /tmp && unzip -a master.zip -d fail2ban && cd fail2ban/$(ls fail2ban)
-command -v python3 >/dev/null 2>&1 || yum install python3
+command -v python3 >/dev/null 2>&1 || yum -y install python3
 python3 setup.py install --record /etc/fail2ban/uninstall.txt
 if [ ! -f /etc/init.d/fail2ban ] ; then cp ./files/redhat-initd /etc/init.d/fail2ban ; sed -i 's/prog=fail2ban-server/prog=fail2ban-server;mkdir -p \/var\/run\/fail2ban/g' /etc/init.d/fail2ban ; chmod u+x,g+x,o+x /etc/init.d/fail2ban ; chkconfig --add --level 345 fail2ban; chkconfig  fail2ban on ; fi
 USR_BIN=$(whereis fail2ban-client | awk -F ':' '{print $2}' | awk -F '/fail2ban-client' '{print $1}' | cut -c 2-)

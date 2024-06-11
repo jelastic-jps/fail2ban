55a77cd46bf8a792f18d9197fff58112753745a6
Dmitry Tsurko <dmytro.tsurko@virtuozzo.com>
2024-06-11 16:15:31 +0300

    JE-71820 Can not install Fail2ban on Almalinux node

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,6 +5,7 @@ cd /tmp && rm -rf fail2ban
 curl -sSfL "https://github.com/fail2ban/fail2ban/archive/master.zip" -o /tmp/master.zip 2>&1
 cd /tmp && unzip -a master.zip -d fail2ban && cd fail2ban/$(ls fail2ban)
 command -v python3 >/dev/null 2>&1 || yum -y install python3
+if grep -a 'AlmaLinux' /etc/system-release ; then rpm -q  python3-setuptools || microdnf -y install python3-setuptools; fi
 python3 setup.py install --record /etc/fail2ban/uninstall.txt
 if [ ! -f /etc/init.d/fail2ban ] ; then cp ./files/redhat-initd /etc/init.d/fail2ban ; sed -i 's/prog=fail2ban-server/prog=fail2ban-server;mkdir -p \/var\/run\/fail2ban/g' /etc/init.d/fail2ban ; chmod u+x,g+x,o+x /etc/init.d/fail2ban ; chkconfig --add --level 345 fail2ban; chkconfig  fail2ban on ; fi
 USR_BIN=$(whereis fail2ban-client | awk -F ':' '{print $2}' | awk -F '/fail2ban-client' '{print $1}' | cut -c 2-)
